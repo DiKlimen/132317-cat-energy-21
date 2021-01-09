bbf3aaddc7dcb462ea0b33c7615dd6613842ee16
Dmitriy <delovar1@gmail.com>
2021-01-09 18:28:37 +0200

    Продолжение вечеринки

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,9 +1,18 @@
-<script type="text/javascript">
-    ymaps.ready(init);
-    function init(){
-        var myMap = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 7
-        });
-    }
-</script>
+ymaps.ready(init);
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [59.938635, 30.323118],
+        zoom: 17
+    });
+}
+
+const toggleBtn = document.querySelector('.main-nav-toggle');
+const nav = document.querySelector('.main-nav');
+
+toggleBtn.classList.remove('hidden');
+nav.classList.remove('active');
+
+toggleBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  toggleBtn.classList.toggle('active');
+});
